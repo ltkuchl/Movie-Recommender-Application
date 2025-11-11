f428ece6cd10ce0238befb98030fc2fd94fab71d
Nils Melén <nils.melen@gmail.com>
2025-11-11 13:34:58 -0500

    Website

--- scr/recommender_item_based.py
+++ scr/recommender_item_based.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the dataset
-movies = pd.read_csv('./data/ml-32m/movies.csv')
-reviews = pd.read_csv('./data/ml-32m/ratings.csv')
-#tags = pd.read_csv('./data/ml-32m/tags.csv')
-#links = pd.read_csv('./data/ml-32m/links.csv')
+movies = pd.read_csv(r'C:\Users\nilsm\OneDrive\Skrivbord\BIG data\ml-32m (1)\ml-32m\movies.csv')
+reviews = pd.read_csv(r'C:\Users\nilsm\OneDrive\Skrivbord\BIG data\ml-32m (1)\ml-32m\ratings.csv')
+tags = pd.read_csv(r'C:\Users\nilsm\OneDrive\Skrivbord\BIG data\ml-32m (1)\ml-32m\tags.csv')
+links = pd.read_csv(r'C:\Users\nilsm\OneDrive\Skrivbord\BIG data\ml-32m (1)\ml-32m\links.csv')
 
 print(movies.head())
+print(reviews.head())
